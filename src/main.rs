c88c05e1e925b260d1a23b68eb6b701d4455e47b
Víctor Oliva <olivarra1@gmail.com>
2023-02-15 00:04:01 +0100

    trying stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use git2::Repository;
+
 fn main() {
     println!("Hello, world!");
+
+    let repo = match Repository::open("./") {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to open: {}", e),
+    };
+
+    let walker = repo.revwalk().unwrap();
+
+    walker.for_each(|c| {
+        let oid = c.unwrap();
+        let commit = repo.find_commit(oid).unwrap();
+        println!("{} {}", oid, commit.message().unwrap());
+    });
+
+    println!("done");
 }
